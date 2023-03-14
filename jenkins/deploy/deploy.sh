b86ff1ccb2dba3ec348b7b2b60e5eaaa0c37cd0f
Sivabala123 <121991288+Sivabala123@users.noreply.github.com>
2023-03-14 22:48:48 +0530

    update

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,4 +4,4 @@ echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth ec2-user@43.204.130.189:/tmp/.auth
+scp -i /opt/prod /tmp/.auth ec2-user@3.108.42.41:/tmp/.auth
